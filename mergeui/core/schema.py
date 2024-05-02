d5fcd0c6555143a7344a6a54a5920f30a3f91f03
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-02 13:37:31 +0200

    update schema: use object graph mapper

--- mergeui/core/schema.py
+++ mergeui/core/schema.py
@@ -1,8 +1,10 @@
 import pydantic as pd
 import typing as t
 import datetime as dt
+import gqlalchemy as gq
+from core.settings import Settings
+from core.db_utils import create_db_connection
 import re
-import stringcase
 
 DataT = t.TypeVar('DataT')
 
@@ -23,116 +25,36 @@ ColumnType = t.Literal[
     "likes", "downloads", "created_at", "updated_at"]
 ExcludeOptionType = t.Literal["base", "merged"]
 SortByOptionType = t.Literal["default", "most likes", "most downloads", "recently created", "recently updated"]
-VarType = int
-
-
-def var_factory(obj: pd.BaseModel) -> VarType:
-    return id(obj)
-
-
-class Node(pd.BaseModel):
-    _var: VarType
-    _labels: list[str]
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        self._var = var_factory(self)
-        labels = []
-        roots = self.__class__.__mro__
-        for root in roots:
-            if root in [Node, pd.BaseModel]:
-                continue
-            if not issubclass(root, pd.BaseModel):
-                continue
-            labels.append(stringcase.pascalcase(root.__name__))
-        labels.reverse()
-        self._labels = labels
-
-    @property
-    def var(self) -> VarType:
-        return self._var
-
-    @property
-    def labels(self) -> list[str]:
-        return self._labels
-
-
-class Relationship(pd.BaseModel):
-    _var: VarType
-    _type: str
-    _var_start: VarType
-    _var_end: VarType
-
-    def __init__(self, start: VarType, end: VarType, **data):
-        super().__init__(**data)
-        self._var = var_factory(self)
-        self._type = stringcase.constcase(stringcase.sentencecase(self.__class__.__name__))
-        self._var_start = start
-        self._var_end = end
-
-    @classmethod
-    def from_nodes(cls, start: Node, end: Node, **data) -> "Relationship":
-        return cls(start=start.var, end=end.var, **data)
-
-    @property
-    def var(self) -> VarType:
-        return self._var
-
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @property
-    def var_start(self) -> VarType:
-        return self._var_start
-
-    @property
-    def var_end(self) -> VarType:
-        return self._var_end
+MergeMethodType = t.Literal[
+    "linear", "slerp", "task_arithmetic", "ties", "dare_ties", "dare_linear", "passthrough",
+    "breadcrumbs", "breadcrumbs_ties", "model_stock", "other"]
 
 
 class Graph(pd.BaseModel):
-    nodes: t.List[Node] = pd.Field(default_factory=list)
-    relationships: t.List[Relationship] = pd.Field(default_factory=list)
+    nodes: t.List[gq.Node] = pd.Field(default_factory=list)
+    relationships: t.List[gq.Relationship] = pd.Field(default_factory=list)
 
 
-class Model(Node):
-    id: str
-    url: t.Optional[pd.AnyHttpUrl] = None
-    name: t.Optional[str] = None
-    description: t.Optional[str] = None
-    license: t.Optional[str] = None
-    author: t.Optional[str] = None
-    merge_method: t.Optional[str] = pd.Field(None, description="Merge strategy")
-    architecture: t.Optional[str] = None
-    likes: t.Optional[int] = None
-    downloads: t.Optional[int] = None
-    created_at: t.Optional[dt.datetime] = None
-    updated_at: t.Optional[dt.datetime] = None
+class Model(gq.Node):
+    # Supported types: bool, int, float, str, list, dict, dt.datetime
+    id: str = gq.Field(index=True, exists=True, unique=True, db=create_db_connection(Settings()))
+    url: t.Optional[str]  # pd.AnyHttpUrl can't be used
+    name: t.Optional[str]
+    description: t.Optional[str]
+    license: t.Optional[str]
+    author: t.Optional[str]
+    merge_method: t.Optional[str] = gq.Field(description="Merge strategy")  # t.Optional[MergeMethodType] can't be used
+    architecture: t.Optional[str]
+    likes: t.Optional[int]
+    downloads: t.Optional[int]
+    created_at: t.Optional[dt.datetime]
+    updated_at: t.Optional[dt.datetime]
 
 
 class MergedModel(Model):
     pass
 
 
-class DerivedFrom(Relationship):
-    origin: str
-    method: str
-
-
-if __name__ == '__main__':
-    g = Graph()
-    n1 = Model(license="MIT", id="me/model1")
-    g.nodes.append(n1)
-    print(n1.dict(exclude_none=True), n1.labels, n1.var)
-    n2 = MergedModel(id="me/model2")
-    g.nodes.append(n2)
-    print(n2.dict(exclude_none=True))
-    r1 = DerivedFrom(var=1, start=n1.var, end=n2.var, origin="me", method="hi")
-    r2 = DerivedFrom.from_nodes(start=n1, end=n2, origin="user", method="hola")
-    g.relationships.append(r1)
-    g.relationships.append(r2)
-    print(r1.dict(exclude_none=True))
-    print(r2.dict(exclude_none=True))
-    print(g.nodes)
-    print(g.relationships)
+class DerivedFrom(gq.Relationship, type="DERIVED_FROM"):
+    origin: str = gq.Field(description="Origin URL")
+    method: str = gq.Field(description="Method used to extract data")
