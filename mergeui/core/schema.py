bc5bb838f304283fbf3564ecdd55c9aa1932c517
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-02 19:05:18 +0200

    fix schema.py

--- mergeui/core/schema.py
+++ mergeui/core/schema.py
@@ -1,12 +1,15 @@
 import pydantic as pd
+import dataclasses as dc
 import typing as t
 import datetime as dt
 import gqlalchemy as gq
 from core.settings import Settings
-from core.db_utils import create_db_connection
+from core.db import DatabaseConnection
 import re
 
 DataT = t.TypeVar('DataT')
+
+db_conn = DatabaseConnection(Settings())
 
 
 class GenericRO(pd.BaseModel, t.Generic[DataT]):
@@ -21,7 +24,7 @@ class GenericRO(pd.BaseModel, t.Generic[DataT]):
 MODEL_ID_REGEX = re.compile(r'^[-\w]+/[-\w]+$')
 
 ColumnType = t.Literal[
-    "id", "name", "description", "license", "author", "merge_method", "architecture",
+    "id", "url", "name", "description", "license", "author", "merge_method", "architecture",
     "likes", "downloads", "created_at", "updated_at"]
 ExcludeOptionType = t.Literal["base", "merged"]
 SortByOptionType = t.Literal["default", "most likes", "most downloads", "recently created", "recently updated"]
@@ -30,14 +33,15 @@ MergeMethodType = t.Literal[
     "breadcrumbs", "breadcrumbs_ties", "model_stock", "other"]
 
 
-class Graph(pd.BaseModel):
-    nodes: t.List[gq.Node] = pd.Field(default_factory=list)
-    relationships: t.List[gq.Relationship] = pd.Field(default_factory=list)
+@dc.dataclass
+class Graph:
+    nodes: t.List[gq.Node] = dc.field(default_factory=list)
+    relationships: t.List[gq.Relationship] = dc.field(default_factory=list)
 
 
 class Model(gq.Node):
     # Supported types: bool, int, float, str, list, dict, dt.datetime
-    id: str = gq.Field(index=True, exists=True, unique=True, db=create_db_connection(Settings()))
+    id: str = gq.Field(index=True, exists=True, unique=True, db=db_conn.db)
     url: t.Optional[str]  # pd.AnyHttpUrl can't be used
     name: t.Optional[str]
     description: t.Optional[str]
