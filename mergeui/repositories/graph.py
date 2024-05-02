29076f27d080e5156a6cdd8f2c7302de12d0a860
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-02 12:35:15 +0200

    add repository: Graph

---
+++ mergeui/repositories/graph.py
@@ -0,0 +1,16 @@
+from core.base import BaseRepository
+import gqlalchemy as gq
+from gqlalchemy.vendors.database_client import DatabaseClient
+from gqlalchemy.query_builders.memgraph_query_builder import Order
+
+
+class GraphRepository(BaseRepository):
+    def __init__(self, db: DatabaseClient):
+        self.db = db
+
+    def get_distinct_property_values(self, key: str = "id", label: str = "") -> list[str]:
+        query = (gq.match()
+                 .node(labels=f"{label}", variable="n")
+                 .return_(f"DISTINCT n.{key} as v")
+                 .order_by(properties=[("v", Order.ASC)]))
+        return list(map(lambda x: x.get("v"), query.execute()))
