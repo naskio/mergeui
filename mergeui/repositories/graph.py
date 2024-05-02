543c5931668cc4820a3786a90f7021f6b76e5d85
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-02 17:24:14 +0200

    graph repository: add get_all_nodes

--- mergeui/repositories/graph.py
+++ mergeui/repositories/graph.py
@@ -1,5 +1,10 @@
 from core.base import BaseRepository
 import gqlalchemy as gq
+import typing as t
+import json
+from core.schema import Model, MergedModel, DerivedFrom, Graph, SortByOptionType, ExcludeOptionType
+from gqlalchemy.query_builders.memgraph_query_builder import Order
+from gqlalchemy.query_builders.memgraph_query_builder import Operator
 from gqlalchemy.vendors.database_client import DatabaseClient
 from gqlalchemy.query_builders.memgraph_query_builder import Order
 
@@ -14,3 +19,15 @@ class GraphRepository(BaseRepository):
              .return_(f"DISTINCT n.{key} as v")
              .order_by(properties=[("v", Order.ASC)]))
         return list(map(lambda x: x.get("v"), q.execute()))
+
+    def get_all_nodes(
+            self,
+            label: str = "",
+    ) -> list[gq.Node]:
+        q = (
+            gq.match()
+            .node(labels=label, variable="n")
+            .return_()
+        )
+        result = list(q.execute())
+        return t.cast(list[gq.Node], result)
