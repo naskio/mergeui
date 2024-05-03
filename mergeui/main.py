d63f9af71f4379bf970a04dbfb2f9f9cbb7c3759
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-03 10:36:55 +0200

    add main for FastAPI with Gradio

---
+++ mergeui/main.py
@@ -0,0 +1,29 @@
+import uvicorn
+import fastapi as fa
+from core.dependencies import get_settings
+from web.api import router as api_router
+
+settings = get_settings()
+
+app = fa.FastAPI(
+    title=settings.app_name,
+    description=f"{settings.description} by {settings.email}",
+)
+
+app.include_router(api_router, prefix="/api", tags=["API"])
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return fa.responses.FileResponse(settings.favicon_path)
+
+
+if not settings.disable_gradio_app:
+    import gradio as gr
+    from web.gradio_app.main import demo
+
+    app = gr.mount_gradio_app(app, demo, path="/", favicon_path=settings.favicon_path, show_error=True)
+
+
+def start_server():
+    uvicorn.run("mergeui.main:app", reload=True, port=8000, log_level="debug")
