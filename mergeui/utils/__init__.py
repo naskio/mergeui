6d7cd732cf192038e37584a7443234093c458895
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-08 10:02:52 +0200

    fix utils

--- mergeui/utils/__init__.py
+++ mergeui/utils/__init__.py
@@ -1,38 +1,63 @@
-import datetime as dt
 import typing as t
+import datetime as dt
 import yaml
 from numerize import numerize
 
 
-def parse_iso_datetime_str(t_str: str) -> dt.datetime:
-    return dt.datetime.strptime(t_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+def naive_to_aware_dt(t_: dt.datetime) -> dt.datetime:
+    """Convert naive datetime to aware datetime in UTC timezone."""
+    if t_.tzinfo is None:
+        t_ = t_.replace(tzinfo=dt.timezone.utc)  # otherwise will take local timezone
+    return t_.astimezone(dt.timezone.utc)
 
 
-def iso_format_datetime(t_: dt.datetime) -> t.Optional[str]:
-    # return f"{t.isoformat(timespec='milliseconds')}Z"
-    if not t_:
-        return None
-    return f"{t_.strftime('%Y-%m-%dT%H:%M:%S')}.000Z"
+def aware_to_naive_dt(t_: dt.datetime) -> dt.datetime:
+    """Convert aware datetime to naive datetime."""
+    if t_.tzinfo is None:
+        return t_
+    t_ = naive_to_aware_dt(t_)  # make sure it's in UTC timezone
+    return t_.replace(tzinfo=None)
 
 
-def format_datetime(t_: dt.datetime) -> t.Optional[str]:
-    if not t_:
-        return None
-    return t_.strftime("%Y-%m-%d %H:%M:%S")
+def pretty_format_dt(t_: t.Optional[dt.datetime]) -> t.Optional[str]:
+    """Format datetime to human-readable string."""
+    if t_ is not None:
+        t_ = naive_to_aware_dt(t_)
+        return t_.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def parse_iso_dt(t_str: t.Optional[str]) -> t.Optional[dt.datetime]:
+    """Parse ISO datetime string (2024-01-25T11:44:11.000Z) to aware datetime object in UTC timezone."""
+    if t_str is not None:
+        t_str = t_str.replace("Z", "")
+        return naive_to_aware_dt(dt.datetime.strptime(t_str, "%Y-%m-%dT%H:%M:%S.%f"))
+
+
+def iso_format_dt(t_: t.Optional[dt.datetime]) -> t.Optional[str]:
+    """Format aware datetime in UTC timezone to ISO datetime string."""
+    if t_ is not None:
+        t_ = naive_to_aware_dt(t_)
+        return f"{t_.strftime('%Y-%m-%dT%H:%M:%S.%f')}Z"
 
 
 def format_large_number(n_: int) -> t.Optional[str]:
+    """Format large number to human-readable string."""
     if n_ is not None:
         return numerize.numerize(n_)
 
 
-def filter_none(d: t.Union[list, dict]) -> t.Union[list, dict]:
+def filter_none(d: t.Union[list, dict, t.Any]) -> t.Union[list, dict, t.Any]:
+    """Filter out None values from a list or dictionary."""
     if isinstance(d, dict):
         return {k: v for k, v in d.items() if v is not None}
-    return [x for x in d if x is not None]
+    elif isinstance(d, list):
+        return [x for x in d if x is not None]
+    else:
+        return d
 
 
 def parse_yaml(yaml_str: str) -> list[t.Union[dict, list]]:
+    """Parse YAML string to list of dictionaries (support also multi-documents yaml string)."""
     try:
         docs = list(yaml.safe_load_all(yaml_str))  # support multi-document yaml
         return [doc for doc in docs if doc is not None]
--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,32 +1,111 @@
 import pytest
+import datetime as dt
 from pathlib import Path
-from core.settings import Settings
-from utils import parse_yaml
-
-settings = Settings()
+from utils import parse_yaml, filter_none, format_large_number, naive_to_aware_dt, aware_to_naive_dt, pretty_format_dt, \
+    parse_iso_dt, iso_format_dt
 
 
-@pytest.fixture(scope="session")
-def mergekit_config_path() -> Path:
-    return settings.project_dir / 'tests/test_data/single_document.yml'
+@pytest.fixture
+def yaml_single_doc_path(settings) -> Path:
+    return settings.project_dir / 'tests/test_data/mergekit_config.yml'
 
 
-@pytest.fixture(scope="session")
-def mergekit_config_path__multi_docs() -> Path:
-    return settings.project_dir / 'tests/test_data/multi_documents.yml'
+@pytest.fixture
+def yaml_multi_docs_path(settings) -> Path:
+    return settings.project_dir / 'tests/test_data/mergekit_config__multi_docs.yml'
 
 
-@pytest.fixture(scope="session")
-def mergekit_moe_config_path() -> Path:
-    return settings.project_dir / 'tests/test_data/mergekit_moe_config.yml'
+@pytest.fixture
+def naive_dt() -> dt.datetime:
+    return dt.datetime(2021, 10, 1, 12, 9, 30, 12345)
 
 
-def test_parse_yaml(mergekit_config_path, mergekit_config_path__multi_docs):
-    yaml_docs = parse_yaml(mergekit_config_path.read_text())
+@pytest.fixture
+def utc_dt() -> dt.datetime:
+    return dt.datetime(2021, 10, 1, 12, 9, 30, 12345, dt.timezone.utc)
+
+
+@pytest.fixture
+def no_utc_dt() -> dt.datetime:
+    return dt.datetime(2021, 10, 1, 13, 9, 30, 12345, dt.timezone(dt.timedelta(hours=1)))
+
+
+@pytest.fixture
+def pretty_dt() -> str:
+    return "2021-10-01 12:09:30"
+
+
+@pytest.fixture
+def iso_dt() -> str:
+    return "2021-10-01T12:09:30.012345Z"
+
+
+def test_parse_yaml(yaml_single_doc_path, yaml_multi_docs_path):
+    yaml_docs = parse_yaml(yaml_single_doc_path.read_text())
     assert yaml_docs is not None
     assert len(yaml_docs) == 1
     assert all([doc is not None for doc in yaml_docs])
-    yaml_docs = parse_yaml(mergekit_config_path__multi_docs.read_text())
+    yaml_docs = parse_yaml(yaml_multi_docs_path.read_text())
     assert yaml_docs is not None
-    assert len(yaml_docs) == 2
+    assert len(yaml_docs) > 1
     assert all([doc is not None for doc in yaml_docs])
+
+
+def test_filter_none():
+    assert filter_none([1, 2, None, 3, None]) == [1, 2, 3]
+    assert filter_none({1: 1, 2: 2, 3: None, 4: None}) == {1: 1, 2: 2}
+    assert filter_none({1: 1, 2: 2, 3: 3, 4: 4}) == {1: 1, 2: 2, 3: 3, 4: 4}
+    assert filter_none([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert filter_none([]) == []
+    assert filter_none({}) == {}
+    assert filter_none(None) is None
+    assert filter_none(1) == 1
+    assert filter_none("1") == "1"
+    assert filter_none(True) is True
+    assert filter_none(False) is False
+    assert filter_none(0) == 0
+    assert filter_none(0.0) == 0.0
+    assert filter_none(0.0j) == 0.0j
+    assert filter_none(0j) == 0j
+
+
+def test_format_large_number():
+    assert format_large_number(9) == '9'
+    assert format_large_number(958) == '958'
+    assert format_large_number(1256) == '1.26K'
+    assert format_large_number(1312000) == '1.31M'
+    assert format_large_number(1000000000) == '1B'
+    assert format_large_number(1000000000000) == '1T'
+    assert format_large_number(1000000000000000) == '1000000000000000'
+
+
+def test_naive_to_aware_dt(naive_dt, utc_dt, no_utc_dt):
+    assert naive_to_aware_dt(naive_dt) == utc_dt
+    assert naive_to_aware_dt(utc_dt) == utc_dt
+    assert naive_to_aware_dt(no_utc_dt) == no_utc_dt
+
+
+def test_aware_to_naive_dt(naive_dt, utc_dt, no_utc_dt):
+    assert aware_to_naive_dt(naive_dt) == naive_dt
+    assert aware_to_naive_dt(utc_dt) == naive_dt
+    assert aware_to_naive_dt(no_utc_dt) == naive_dt
+
+
+def test_pretty_format_dt(naive_dt, utc_dt, no_utc_dt, pretty_dt):
+    assert pretty_format_dt(None) is None
+    assert pretty_format_dt(naive_dt) == pretty_dt
+    assert pretty_format_dt(utc_dt) == pretty_dt
+    assert pretty_format_dt(no_utc_dt) == pretty_dt
+
+
+def test_parse_iso_dt(utc_dt, iso_dt, naive_dt):
+    parsed_dt = parse_iso_dt(iso_dt)
+    assert parsed_dt != naive_dt
+    assert parsed_dt.tzinfo is dt.timezone.utc
+    assert parsed_dt == utc_dt
+
+
+def test_iso_format_dt(utc_dt, iso_dt, naive_dt):
+    assert iso_format_dt(None) is None
+    assert iso_format_dt(naive_dt) == iso_dt
+    assert iso_format_dt(utc_dt) == iso_dt
