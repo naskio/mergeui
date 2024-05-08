c34f073a76ca1a940f275068bdf1c39340fbc40b
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-08 10:13:34 +0200

    improve graph repository

--- mergeui/repositories/graph.py
+++ mergeui/repositories/graph.py
@@ -3,8 +3,8 @@ import gqlalchemy as gq
 from gqlalchemy.connection import _convert_memgraph_value
 from gqlalchemy.query_builders.memgraph_query_builder import Operator
 from gqlalchemy.query_builders.memgraph_query_builder import Order
+import core.db
 from core.base import BaseRepository
-from core.db import DatabaseConnection
 from core.schema import Model, Graph
 
 
@@ -15,18 +15,18 @@ def _get_where_clause(q, where_initiated: bool):
 def _results_as_graph(results) -> Graph:
     if not results:
         return Graph()
-    assert len(results) == 1, "Multiple results returned from query"
+    assert len(results) == 1, "Multiple results returned from query but only one expected."
     nodes = list(map(_convert_memgraph_value, results[0]["nodes"]))
     relationships = list(map(_convert_memgraph_value, results[0]["relationships"]))
     return Graph(nodes=nodes, relationships=relationships)
 
 
 class GraphRepository(BaseRepository):
-    def __init__(self, db_conn: DatabaseConnection):
+    def __init__(self, db_conn: 'core.db.DatabaseConnection'):
         self.db_conn = db_conn
 
     def list_property_values(self, key: str = "id", label: str = "") -> list[str]:
-        """Get all possible values for a property"""
+        """Get all possible values for a property including None"""
         q = (gq.match(connection=self.db_conn.db)
              .node(labels=label, variable="n")
              .return_(f"DISTINCT n.{key} as v")
@@ -88,7 +88,7 @@ class GraphRepository(BaseRepository):
             q = _get_where_clause(q, where_initiated)("n.architecture", Operator.EQUAL, literal=architecture)
             where_initiated = True
         # search query
-        if query is not None:
+        if query is not None and query.strip() != "":
             q = (
                 q.with_(
                     "n",
@@ -105,16 +105,26 @@ class GraphRepository(BaseRepository):
         result = list(map(lambda x: x.get("n"), q.execute()))
         return t.cast(list[Model], result)
 
-    def get_sub_graph(self, id_: str, label: str = "", max_depth: t.Optional[int] = None) -> Graph:
+    def get_sub_graph(
+            self,
+            id_: str,
+            label: str = "",
+            relationship_type: str = "",
+            max_depth: t.Optional[int] = None,
+    ) -> Graph:
         """Get a sub-graph from a starting node"""
         if not id_:
             return Graph()
         q = (gq.match(connection=self.db_conn.db)
              .node(label, variable="n", id=id_))
-        if max_depth is not None:
-            q = q.to(variable=f"r*..{max_depth}", directed=False)
+        if relationship_type:
+            rel_var = f"r:{relationship_type}"
         else:
-            q = q.to(variable="r*", directed=False)
+            rel_var = "r"
+        if max_depth is not None:
+            q = q.to(variable=f"{rel_var}*..{max_depth}", directed=False)
+        else:
+            q = q.to(variable=f"{rel_var}*", directed=False)
         q = (
             q.node(label, variable="m")
             .with_("COLLECT(n)+COLLECT(m) AS all_nodes")
--- tests/test_repositories/test_graph.py
+++ tests/test_repositories/test_graph.py
@@ -1,112 +1,105 @@
-import sys
-from loguru import logger
 import gqlalchemy as gq
-from core.db import DatabaseConnection, Settings
-from repositories.graph import GraphRepository
-
-logger.remove()
-logger.add(sink=sys.stderr, level='INFO')
 
 
-class TestGraphRepository:
-    @classmethod
-    def setup_class(cls):
-        cls.settings = Settings()
-        cls.db_conn = DatabaseConnection(cls.settings)
-        # cls.teardown_class()
-        cls.db_conn.setup()
-        cls.repository = GraphRepository(cls.db_conn)
-        cls.db_conn.populate_from_json(cls.settings.project_dir / "tests/test_data/graph.json")
+def test_list_property_values(graph_repository):
+    choices = graph_repository.list_property_values("license")
+    assert isinstance(choices, list)
+    assert "apache-2.0" in choices
+    assert None not in choices
+    assert choices == sorted(choices)
 
-    @classmethod
-    def teardown_class(cls):
-        cls.db_conn.reset()
 
-    def test_list_property_values(self):
-        license_choices = self.repository.list_property_values('license')
-        assert 'apache-2.0' in license_choices
-        assert license_choices == sorted(license_choices)
+def test_list_property_values__none(graph_repository):
+    choices = graph_repository.list_property_values("merge_method")
+    assert isinstance(choices, list)
+    assert "slerp" in choices
+    assert None in choices
 
-    def test_list_nodes(self):
-        result = self.repository.list_nodes()
-        assert len(result) == 6
-        assert isinstance(result[0], gq.Node)
-        # limit
-        result = self.repository.list_nodes(limit=2)
-        assert len(result) == 2
 
-    def test_list_models(self):
-        # get all
-        result = self.repository.list_models()
-        assert len(result) == 6
-        assert isinstance(result[0], gq.Node)
-        # limit
-        result = self.repository.list_models(limit=2)
-        assert len(result) == 2
-        # get by license only
-        result = self.repository.list_models(license_='cc-by-nc-4.0')
-        assert len(result) == 1
-        # get by search query only
-        result = self.repository.list_models('cc-by-nc')
-        assert len(result) == 1
-        # get by license and architecture
-        result = self.repository.list_models(license_='cc-by-nc-4.0', architecture="MistralForCausalLM")
-        assert len(result) == 1
-        result = self.repository.list_models(license_='cc-by-nc-4.0', architecture="NotFound")
-        assert len(result) == 0
-        # exclude
-        result = self.repository.list_models(license_='apache-2.0')
-        assert len(result) == 5
-        result = self.repository.list_models(license_='apache-2.0', label="MergedModel")
-        assert len(result) == 1
-        result = self.repository.list_models(license_='apache-2.0', not_label="MergedModel")
-        assert len(result) == 4
-        # base_model
-        result = self.repository.list_models(license_='apache-2.0', label="MergedModel",
-                                             base_model="fblgit/una-cybertron-7b-v2-bf16")
-        assert len(result) == 1
-        result = self.repository.list_models(
-            license_='apache-2.0', label="MergedModel",
-            base_model="mistralai/Mistral-7B-v0.1")
-        assert len(result) == 0
-        # get by search query only
-        result = self.repository.list_models('hola')
-        assert len(result) == 0
-        result = self.repository.list_models('hola', license_='apache-2.0')
-        assert len(result) == 0
-        # search query with license
-        result = self.repository.list_models('cc-by-nc', license_='cc-by-nc-4.0')
-        assert len(result) == 1
-        result = self.repository.list_models('MistralForCausalLM', license_='apache-2.0')
-        assert len(result) == 5
-        # merge_method
-        result = self.repository.list_models(merge_method='slerp')
-        assert len(result) == 2
+def test_list_nodes(graph_repository):
+    result = graph_repository.list_nodes()
+    assert len(result) == 6
+    assert isinstance(result[0], gq.Node)
 
-    def test_get_sub_graph(self):
-        # top node
-        gr = self.repository.get_sub_graph('Q-bert/MetaMath-Cybertron-Starling')
-        assert len(gr.nodes) == 5
-        assert isinstance(gr.nodes[0], gq.Node)
-        assert len(gr.relationships) == 7
-        assert isinstance(gr.relationships[0], gq.Relationship)
-        # isolated node
-        gr = self.repository.get_sub_graph('mistralai/Mistral-7B-v0.1')
-        assert len(gr.nodes) == 1
-        assert len(gr.relationships) == 0
-        # invalid node
-        gr = self.repository.get_sub_graph('x/y')
-        assert len(gr.nodes) == 0
-        assert len(gr.relationships) == 0
-        # middle node
-        gr = self.repository.get_sub_graph('Q-bert/MetaMath-Cybertron')
-        assert len(gr.nodes) == 5
-        assert len(gr.relationships) == 7
-        # bottom node
-        gr = self.repository.get_sub_graph('berkeley-nest/Starling-LM-7B-alpha')
-        assert len(gr.nodes) == 5
-        assert len(gr.relationships) == 7
-        # empty id
-        gr = self.repository.get_sub_graph('')
-        assert len(gr.nodes) == 0
-        assert len(gr.relationships) == 0
+
+def test_list_nodes__limit(graph_repository):
+    result = graph_repository.list_nodes(limit=2)
+    assert len(result) == 2
+
+
+def test_list_models(graph_repository):
+    # get all
+    result = graph_repository.list_models()
+    assert len(result) == 6
+    assert isinstance(result[0], gq.Node)
+    # limit
+    result = graph_repository.list_models(limit=2)
+    assert len(result) == 2
+    # get by license only
+    result = graph_repository.list_models(license_='cc-by-nc-4.0')
+    assert len(result) == 1
+    # get by search query only
+    result = graph_repository.list_models('cc-by-nc')
+    assert len(result) == 1
+    # get by license and architecture
+    result = graph_repository.list_models(license_='cc-by-nc-4.0', architecture="MistralForCausalLM")
+    assert len(result) == 1
+    result = graph_repository.list_models(license_='cc-by-nc-4.0', architecture="NotFound")
+    assert len(result) == 0
+    # exclude
+    result = graph_repository.list_models(license_='apache-2.0')
+    assert len(result) == 5
+    result = graph_repository.list_models(license_='apache-2.0', label="MergedModel")
+    assert len(result) == 1
+    result = graph_repository.list_models(license_='apache-2.0', not_label="MergedModel")
+    assert len(result) == 4
+    # base_model
+    result = graph_repository.list_models(license_='apache-2.0', label="MergedModel",
+                                          base_model="fblgit/una-cybertron-7b-v2-bf16")
+    assert len(result) == 1
+    result = graph_repository.list_models(
+        license_='apache-2.0', label="MergedModel",
+        base_model="mistralai/Mistral-7B-v0.1")
+    assert len(result) == 0
+    # get by search query only
+    result = graph_repository.list_models('hola')
+    assert len(result) == 0
+    result = graph_repository.list_models('hola', license_='apache-2.0')
+    assert len(result) == 0
+    # search query with license
+    result = graph_repository.list_models('cc-by-nc', license_='cc-by-nc-4.0')
+    assert len(result) == 1
+    result = graph_repository.list_models('MistralForCausalLM', license_='apache-2.0')
+    assert len(result) == 5
+    # merge_method
+    result = graph_repository.list_models(merge_method='slerp')
+    assert len(result) == 2
+
+
+def test_get_sub_graph(graph_repository):
+    # top node
+    gr = graph_repository.get_sub_graph('Q-bert/MetaMath-Cybertron-Starling')
+    assert len(gr.nodes) == 5
+    assert isinstance(gr.nodes[0], gq.Node)
+    assert len(gr.relationships) == 7
+    assert isinstance(gr.relationships[0], gq.Relationship)
+    # isolated node
+    gr = graph_repository.get_sub_graph('mistralai/Mistral-7B-v0.1')
+    assert len(gr.nodes) == 1
+    assert len(gr.relationships) == 0
+    # invalid node
+    gr = graph_repository.get_sub_graph('x/y')
+    assert len(gr.nodes) == 0
+    assert len(gr.relationships) == 0
+    # middle node
+    gr = graph_repository.get_sub_graph('Q-bert/MetaMath-Cybertron')
+    assert len(gr.nodes) == 5
+    assert len(gr.relationships) == 7
+    # bottom node
+    gr = graph_repository.get_sub_graph('berkeley-nest/Starling-LM-7B-alpha')
+    assert len(gr.nodes) == 5
+    assert len(gr.relationships) == 7
+    # empty id
+    gr = graph_repository.get_sub_graph('')
+    assert len(gr.nodes) == 0
+    assert len(gr.relationships) == 0
