c6aec1488f42caba315e7c88622179adb97b2cf1
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-12 07:01:44 +0200

    improving graph repository

--- mergeui/repositories/graph.py
+++ mergeui/repositories/graph.py
@@ -8,10 +8,6 @@ from core.base import BaseRepository
 from core.schema import Model, Graph
 
 
-def _get_where_clause(q, where_initiated: bool):
-    return getattr(q, "where" if not where_initiated else "and_where")
-
-
 def _results_as_graph(results) -> Graph:
     if not results:
         return Graph()
@@ -25,12 +21,18 @@ class GraphRepository(BaseRepository):
     def __init__(self, db_conn: 'core.db.DatabaseConnection'):
         self.db_conn = db_conn
 
-    def list_property_values(self, *, key: str = "id", label: str = "") -> list[str]:
+    def list_property_values(self, *, key: str = "id", label: str = "", exclude_none: bool = False) -> list[str]:
         """Get all possible values for a property including None"""
-        q = (gq.match(connection=self.db_conn.db)
-             .node(labels=label, variable="n")
-             .return_(f"DISTINCT n.{key} as v")
-             .order_by(properties=[("v", Order.ASC)]))
+        q = (
+            gq.match(connection=self.db_conn.db)
+            .node(labels=label, variable="n")
+        )
+        if exclude_none:
+            q = q.add_custom_cypher(f"WHERE n.{key} IS NOT NULL")
+        q = (
+            q.return_(f"DISTINCT n.{key} as v")
+            .order_by(properties=[("v", Order.ASC)])
+        )
         return list(map(lambda x: x.get("v"), q.execute()))
 
     def list_nodes(
@@ -38,12 +40,12 @@ class GraphRepository(BaseRepository):
             *,
             label: str = "",
             limit: t.Optional[int] = None,
-            **kwargs,
+            filters: t.Optional[dict[str, t.Any]] = None,
     ) -> list[gq.Node]:
         """Get all nodes with a specific label"""
         q = (
             gq.match(connection=self.db_conn.db)
-            .node(labels=label, variable="n", **(kwargs or {}))
+            .node(labels=label, variable="n", **(filters or {}))
             .return_("DISTINCT n")
         )
         if limit is not None:
@@ -51,81 +53,20 @@ class GraphRepository(BaseRepository):
         result = list(map(lambda x: x.get("n"), q.execute()))
         return t.cast(list[gq.Node], result)
 
-    def list_models(
-            self,
-            *,
-            query: t.Optional[str] = None,
-            label: str = "Model",
-            not_label: t.Optional[str] = None,
-            sort_key: t.Optional[str] = None,
-            sort_order: t.Optional[Order] = None,
-            exclude_null_on_sort_key: bool = False,
-            license_: t.Optional[str] = None,
-            merge_method: t.Optional[str] = None,
-            architecture: t.Optional[str] = None,
-            base_model: t.Optional[str] = None,
-            limit: t.Optional[int] = None,
-    ) -> list[Model]:
-        """Get all models with optional filters"""
-        q = gq.match(connection=self.db_conn.db)
-        where_initiated = False
-        # label
-        q = q.node(label, variable="n")
-        # base_model
-        if base_model is not None:
-            q = (q.to("DERIVED_FROM", variable="r")
-                 .node("Model", variable="m", id=base_model))
-        # not_label
-        if not_label is not None:
-            q = q.where_not("n", Operator.LABEL_FILTER, expression=not_label)
-            where_initiated = True
-        # license
-        if license_ is not None:
-            q = _get_where_clause(q, where_initiated)("n.license", Operator.EQUAL, literal=license_)
-            where_initiated = True
-        # merge_method
-        if merge_method is not None:
-            q = _get_where_clause(q, where_initiated)("n.merge_method", Operator.EQUAL, literal=merge_method)
-            where_initiated = True
-        # architecture
-        if architecture is not None:
-            q = _get_where_clause(q, where_initiated)("n.architecture", Operator.EQUAL, literal=architecture)
-            where_initiated = True
-        # exclude null on sort_key
-        if exclude_null_on_sort_key and sort_key is not None:
-            q = q.add_custom_cypher(f"{'AND' if where_initiated else 'WHERE'} n.{sort_key} IS NOT NULL")
-            where_initiated = True
-        # search query
-        if query is not None and query.strip() != "":
-            q = (
-                q.with_(
-                    "n",
-                ).call("text_search.search_all", f"'{self.db_conn.settings.text_index_name}', '{query}'")
-                .yield_("node")
-                .with_("n, node")
-                .where("n", Operator.EQUAL, expression="node")
-            )
-        q = q.return_(f"DISTINCT n")
-        if sort_key is not None:
-            q = q.order_by(properties=[(f"n.{sort_key}", sort_order or Order.ASC)])
-        if limit is not None:
-            q = q.limit(limit)
-        result = list(map(lambda x: x.get("n"), q.execute()))
-        return t.cast(list[Model], result)
-
     def get_sub_graph(
             self,
             *,
-            id_: str,
+            start_id: str,
             label: str = "",
             relationship_type: str = "",
             max_depth: t.Optional[int] = None,
     ) -> Graph:
         """Get a sub-graph from a starting node"""
-        if not id_:
+        if not start_id:
             return Graph()
+        # get nodes
         q = (gq.match(connection=self.db_conn.db)
-             .node(label, variable="n", id=id_))
+             .node(label, variable="n", id=start_id))
         if relationship_type:
             rel_var = f"r:{relationship_type}"
         else:
@@ -139,9 +80,12 @@ class GraphRepository(BaseRepository):
             .with_("COLLECT(n)+COLLECT(m) AS all_nodes")
             .unwind("all_nodes", variable="node")
             .with_("COLLECT(DISTINCT node) AS distinct_nodes")
-            .match()
+        )
+        # get relationships
+        q = (
+            q.match()
             .node(label, variable="a")
-            .to(variable="rel")
+            .to(relationship_type=relationship_type, variable="rel")
             .node(label, variable="b")
             .add_custom_cypher("WHERE a IN distinct_nodes AND b IN distinct_nodes")
             .with_("COLLECT(DISTINCT rel) AS distinct_rels, distinct_nodes")
@@ -149,10 +93,10 @@ class GraphRepository(BaseRepository):
         )
         results = list(q.execute())
         gr = _results_as_graph(results)
-        if not gr.nodes:  # handle isolated node case
+        if not gr.nodes:  # handle isolated node or empty graph
             q = (
                 gq.match(connection=self.db_conn.db)
-                .node(label, variable="n", id=id_)
+                .node(label, variable="n", id=start_id)
                 .return_("COLLECT(DISTINCT n) AS nodes, [] AS relationships")
             )
             results = list(q.execute())
--- tests/test_repositories/test_graph.py
+++ tests/test_repositories/test_graph.py
@@ -16,6 +16,13 @@ def test_list_property_values__none(graph_repository):
     assert None in choices
 
 
+def test_list_property_values__exclude_none(graph_repository):
+    choices = graph_repository.list_property_values(key="merge_method", exclude_none=True)
+    assert isinstance(choices, list)
+    assert "slerp" in choices
+    assert None not in choices
+
+
 def test_list_nodes(graph_repository):
     result = graph_repository.list_nodes()
     assert len(result) == 6
@@ -27,79 +34,35 @@ def test_list_nodes__limit(graph_repository):
     assert len(result) == 2
 
 
-def test_list_models(graph_repository):
-    # get all
-    result = graph_repository.list_models()
-    assert len(result) == 6
-    assert isinstance(result[0], gq.Node)
-    # limit
-    result = graph_repository.list_models(limit=2)
-    assert len(result) == 2
-    # get by license only
-    result = graph_repository.list_models(license_='cc-by-nc-4.0')
-    assert len(result) == 1
-    # get by search query only
-    result = graph_repository.list_models(query='cc-by-nc')
-    assert len(result) == 1
-    # get by license and architecture
-    result = graph_repository.list_models(license_='cc-by-nc-4.0', architecture="MistralForCausalLM")
-    assert len(result) == 1
-    result = graph_repository.list_models(license_='cc-by-nc-4.0', architecture="NotFound")
-    assert len(result) == 0
-    # exclude
-    result = graph_repository.list_models(license_='apache-2.0')
-    assert len(result) == 5
-    result = graph_repository.list_models(license_='apache-2.0', label="MergedModel")
-    assert len(result) == 1
-    result = graph_repository.list_models(license_='apache-2.0', not_label="MergedModel")
-    assert len(result) == 4
-    # base_model
-    result = graph_repository.list_models(license_='apache-2.0', label="MergedModel",
-                                          base_model="fblgit/una-cybertron-7b-v2-bf16")
-    assert len(result) == 1
-    result = graph_repository.list_models(
-        license_='apache-2.0', label="MergedModel",
-        base_model="mistralai/Mistral-7B-v0.1")
-    assert len(result) == 0
-    # get by search query only
-    result = graph_repository.list_models(query='hola')
-    assert len(result) == 0
-    result = graph_repository.list_models(query='hola', license_='apache-2.0')
-    assert len(result) == 0
-    # search query with license
-    result = graph_repository.list_models(query='cc-by-nc', license_='cc-by-nc-4.0')
-    assert len(result) == 1
-    result = graph_repository.list_models(query='MistralForCausalLM', license_='apache-2.0')
-    assert len(result) == 5
-    # merge_method
-    result = graph_repository.list_models(merge_method='slerp')
+def test_list_nodes__filters(graph_repository):
+    result = graph_repository.list_nodes(filters=dict(merge_method="slerp"))
     assert len(result) == 2
 
 
 def test_get_sub_graph(graph_repository):
     # top node
-    gr = graph_repository.get_sub_graph(id_='Q-bert/MetaMath-Cybertron-Starling')
+    gr = graph_repository.get_sub_graph(start_id='Q-bert/MetaMath-Cybertron-Starling')
     assert len(gr.nodes) == 5
     assert isinstance(gr.nodes[0], gq.Node)
     assert len(gr.relationships) == 7
     assert isinstance(gr.relationships[0], gq.Relationship)
     # isolated node
-    gr = graph_repository.get_sub_graph(id_='mistralai/Mistral-7B-v0.1')
+    gr = graph_repository.get_sub_graph(start_id='mistralai/Mistral-7B-v0.1')
     assert len(gr.nodes) == 1
     assert len(gr.relationships) == 0
     # invalid node
-    gr = graph_repository.get_sub_graph(id_='x/y')
+    gr = graph_repository.get_sub_graph(start_id='x/y')
     assert len(gr.nodes) == 0
     assert len(gr.relationships) == 0
     # middle node
-    gr = graph_repository.get_sub_graph(id_='Q-bert/MetaMath-Cybertron')
+    gr = graph_repository.get_sub_graph(start_id='Q-bert/MetaMath-Cybertron')
     assert len(gr.nodes) == 5
     assert len(gr.relationships) == 7
     # bottom node
-    gr = graph_repository.get_sub_graph(id_='berkeley-nest/Starling-LM-7B-alpha')
+    gr = graph_repository.get_sub_graph(start_id='berkeley-nest/Starling-LM-7B-alpha')
     assert len(gr.nodes) == 5
     assert len(gr.relationships) == 7
     # empty id
-    gr = graph_repository.get_sub_graph(id_='')
+    gr = graph_repository.get_sub_graph(start_id='')
     assert len(gr.nodes) == 0
     assert len(gr.relationships) == 0
