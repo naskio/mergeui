ec5d107e528108ff3014cfb6083b829fba910864
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-21 16:54:10 +0200

    add CLI: reset_text_search_index
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

---
+++ mergeui/cli/reset_text_search_index.py
@@ -0,0 +1,10 @@
+from core.dependencies import get_model_repository
+
+
+def main():
+    model_repository = get_model_repository()
+    model_repository.drop_text_search_index()
+
+
+if __name__ == '__main__':
+    main()
