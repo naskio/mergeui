ec1b5cd1b23b2463f8cba4cb30412d09dd02d315
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-22 08:56:21 +0200

    fix gr functions
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- mergeui/web/gradio_app/functions.py
+++ mergeui/web/gradio_app/functions.py
@@ -2,7 +2,7 @@ import typing as t
 from loguru import logger
 import gradio as gr
 from gradio.components import Component as BaseGradioComponent
-from core.dependencies import get_model_service, get_settings
+from core.dependencies import get_model_service
 from core.schema import BaseValidationError, SortByOptionType, ExcludeOptionType, DisplayColumnType
 from web.schema import GenericRO, GetModelLineageInputDTO, ListModelsInputDTO, LabelFieldType, ColorFieldType, \
     DataFrameDataType
@@ -12,22 +12,27 @@ from utils.graph_viz import Plot, GraphPlotBuilder
 
 def get_model_lineage(
         model_id: str,
+        directed: bool,
+        max_hops: int,
         label_field: t.Optional[LabelFieldType],
         color_field: t.Optional[ColorFieldType],
 ) -> t.Tuple[BaseGradioComponent, BaseGradioComponent]:
     """Get the lineage of a model as gr.Plot."""
-    logger.trace(f"get_model_lineage: `{model_id}`,`{label_field}`,`{color_field}`")
+    logger.trace(f"get_model_lineage: `{model_id}`,`{directed}`,`{max_hops}`,`{label_field}`,`{color_field}`")
     try:
+        # validate input
         inp = GetModelLineageInputDTO(
             id=fix_gradio_select_value(model_id, ""),
+            directed=directed,
+            max_hops=max_hops,
             label_field=fix_gradio_select_value(label_field),
             color_field=fix_gradio_select_value(color_field),
-        )  # validate input
-        settings = get_settings()
+        )
         model_service = get_model_service()
         graph = model_service.get_model_lineage(
             model_id=inp.id,
-            max_depth=settings.max_graph_depth
+            directed=inp.directed,
+            max_hops=inp.max_hops,
         )
         plot = GraphPlotBuilder(
             graph=graph,
@@ -44,41 +49,43 @@ def get_model_lineage(
 
 
 def list_models(
-        query: str,
-        sort_by: SortByOptionType,
+        query: t.Optional[str],
+        sort_by: t.Optional[SortByOptionType],
         display_columns: t.List[DisplayColumnType],
-        exclude: ExcludeOptionType,
-        license_: str,
-        base_model: str,
-        merge_method: str,
-        architecture: str,
+        excludes: t.List[ExcludeOptionType],
+        author: t.Optional[str],
+        license_: t.Optional[str],
+        merge_method: t.Optional[str],
+        architecture: t.Optional[str],
+        base_model: t.Optional[str],
 ) -> t.Tuple[BaseGradioComponent, BaseGradioComponent]:
     """List models in a gr.DataFrame."""
-    logger.trace(f"list_models: `{query}`,`{sort_by}`,`{display_columns}`,`{exclude}`,`{license_}`,"
-                 f"`{base_model}`,`{merge_method}`,`{architecture}`")
+    logger.trace(f"list_models: `{query}`,`{sort_by}`,`{display_columns}`,`{excludes}`,`{author}`,`{license_}`,"
+                 f"`{merge_method}`,`{architecture}`,`{base_model}`")
     try:
         # validate input
         inp = ListModelsInputDTO(
             query=query,
             sort_by=fix_gradio_select_value(sort_by),
             display_columns=display_columns,
-            exclude=fix_gradio_select_value(exclude),
+            excludes=excludes,
+            author=fix_gradio_select_value(author),
             license=fix_gradio_select_value(license_),
-            base_model=fix_gradio_select_value(base_model),
             merge_method=fix_gradio_select_value(merge_method),
             architecture=fix_gradio_select_value(architecture),
+            base_model=fix_gradio_select_value(base_model),
         )
-        settings = get_settings()
         model_service = get_model_service()
         models = model_service.list_models(
             query=inp.query,
             sort_by=inp.sort_by,
-            exclude=inp.exclude,
+            excludes=inp.excludes,
+            author=inp.author,
             license_=inp.license,
             merge_method=inp.merge_method,
             architecture=inp.architecture,
             base_model=inp.base_model,
-            limit=settings.results_limit,
+            limit=inp.limit,
         )
         dfd: DataFrameDataType = models_as_dataframe(models, inp.display_columns, pretty=True)
         ro = GenericRO[DataFrameDataType](data=dfd)
@@ -89,6 +96,7 @@ def list_models(
     if not ro.data[0]:
         return gr.DataFrame(visible=False), gr.Label(
             f"No results found, please try different keywords/filters.", visible=True, label="INFO", show_label=True)
+    # noinspection PyTypeChecker
     return gr.DataFrame(value=ro.data[0], headers=ro.data[1], datatype=ro.data[2],
                         visible=True, wrap=True, line_breaks=True,
                         label=f"Models ({len(ro.data[0])})", show_label=True), gr.Label(visible=False)
