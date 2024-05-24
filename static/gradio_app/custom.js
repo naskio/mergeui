da363534186263b2ad742fa6f1db7a304992c02f
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2024-05-24 15:01:20 +0200

    move contribute CTA to footer
    Signed-off-by: Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>

--- static/gradio_app/custom.js
+++ static/gradio_app/custom.js
@@ -1,3 +1,10 @@
-function forceDisableDarkMode() {
+function () {
+    // forceDisableDarkMode
     document.body.classList.remove("dark");
+    // update footer
+    const footer = document.querySelector("footer");
+    footer.innerHTML = `<small style="color: dimgray; margin: 2px;">
+        MergeUI is a new project and any <a href="https://github.com/naskio/mergeui" style="color: #f43f5d;" target="_blank">
+        contribution 🤗</a> would make a difference!
+        </small>`;
 }
